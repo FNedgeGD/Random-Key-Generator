3c798b62e6683f03ddc721e48f91e5ef9cdf2ba2
FNedgeGD <1690263814@qq.com>
2025-02-22 11:26:02 +0800

    refactor random library added

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,32 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <ctime>
+#include <random>
 using namespace std;
 
 int main(){
-	srand(time(nullptr));
-	int type;
-	char temp;
+	default_random_engine engine;
+	uniform_int_distribution<int> type(0,99);
+	uniform_int_distribution<int> number(0,9);
+	uniform_int_distribution<char> caps('A','Z');
+	uniform_int_distribution<char> lower('a','z');
+	engine.seed(time(0));
 //	while(true){
-		system("pause");
-		cout<<endl<<"---------------------------------------------------\nThe KEY is: ";
-		for(int i=1 ; i<=16; i++) {
-			type=(rand()%(99-1+1))+99;
-			if(type%3==1) { //num
-				cout<<(rand()%(100-0+1))%10;
-			}
-			else if(type%3==2){ //caps
-				temp=(rand()%(260-0+1))%26+65;
-				cout<<temp;
-			}
-			else if(type%3==0){ //lit
-				temp=(rand()%(260-0+1))%26+97;
-				cout<<temp;
+		system ( "pause" );
+		printf ( "\n---------------------------------------------------\nThe KEY is: " );
+		for ( int i = 1 ; i <= 16; i++ ) {
+			switch (type(engine) % 3) {
+			case 0:
+				printf("%d", number(engine)); //num
+				break;
+			case 1:
+				printf("%c", caps(engine)); //caps
+				break;
+			case 2:
+				printf("%c", lower(engine)); //lit
+				break;
 			}
 		}
-		cout<<endl<<"---------------------------------------------------\n";
+		printf("\n---------------------------------------------------\n");
 //	}
 	return 0;
 }
